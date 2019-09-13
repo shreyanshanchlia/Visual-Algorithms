47b8d50c177ece235247e6261f94785ae8eb8cc3
shreyanshanchlia <48558967+shreyanshanchlia@users.noreply.github.com>
2019-09-13 23:23:21 +0530

    BubbleSort lines

---
+++ Assets/Sort.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sort : MonoBehaviour
+{
+	public GameObject parentLines;
+	private Vector3 forSwapTransform;
+    public void BubbleSort()
+	{
+		StartCoroutine(BubbleSortCoroutine());
+	}
+	IEnumerator BubbleSortCoroutine()
+	{
+		int n = parentLines.transform.childCount;
+		for (int i = 0; i < n - 1; i++)
+		{
+			for (int j = 0; j < n - i - 1; j++)
+			{
+				if (parentLines.transform.GetChild(j).localScale.y < parentLines.transform.GetChild(j + 1).localScale.y)
+				{
+					forSwapTransform = parentLines.transform.GetChild(j).localScale;
+					parentLines.transform.GetChild(j).localScale = parentLines.transform.GetChild(j+1).localScale;
+					parentLines.transform.GetChild(j + 1).localScale = forSwapTransform;
+					yield return null;
+				}
+			}
+		}
+	}
+}
