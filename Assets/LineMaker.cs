ea2fe7f33fb1f6e04f44411cad1798180db16fae
shreyanshanchlia <48558967+shreyanshanchlia@users.noreply.github.com>
2019-09-13 22:17:01 +0530

    generate lines

---
+++ Assets/LineMaker.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class LineMaker : MonoBehaviour
+{
+	public GameObject linePrefab;
+	public GameObject parentLines;
+	private GameObject instantiatedLine;
+	private Vector3 childTransform;
+	private Vector3 childScale;
+	private int height;
+	private float thickness;
+
+	public void MakeLines(float noOfLines)
+	{
+		foreach (Transform child in parentLines.transform)
+		{
+			GameObject.Destroy(child.gameObject);
+		}
+		childTransform = Vector3.zero;
+		childScale = Vector3.zero;
+		thickness = 10.0f;
+		for (int i = 0; i < noOfLines; i++)
+		{
+			height = Random.Range(5, 50);
+			instantiatedLine = Instantiate(linePrefab, parentLines.transform);
+			childTransform.x = thickness * ((noOfLines / 2) - i);
+			instantiatedLine.transform.localPosition = childTransform;
+			childScale.x = 1000;
+			childScale.y = height*250;
+			instantiatedLine.transform.localScale = childScale;
+		}
+	}
+}
