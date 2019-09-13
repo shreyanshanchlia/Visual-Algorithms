0b01493c8bafd5e53cdba73573cce9e2cad3b164
shreyanshanchlia <48558967+shreyanshanchlia@users.noreply.github.com>
2019-09-14 04:21:01 +0530

    higher value support
    values can now go higher than 50.

--- Assets/LineMaker.cs
+++ Assets/LineMaker.cs
@@ -11,11 +11,10 @@ public class LineMaker : MonoBehaviour
 	private Vector3 childTransform;
 	private Vector3 childScale;
 	private int height;
+	private float heightMultiplier;
 	private float thickness;
-	private int[] shuffledNumbers = new int[50];
-	List<int> numbersList = new List<int>(50);
 	
-	public void MakeLines(float noOfLines)
+	public void MakeLinesRepeatOn(float noOfLines)
 	{
 		Sort.instance.StopAllCoroutines();
 		foreach (Transform child in parentLines.transform)
@@ -25,13 +24,17 @@ public class LineMaker : MonoBehaviour
 		childTransform = Vector3.zero;
 		childScale = Vector3.zero;
 		thickness = 10.0f;
+		if(thickness * 1.05f * noOfLines > 472.5f)
+		{
+			thickness = 472.5f / (noOfLines * 1.05f);
+		}
 		for (int i = (int)noOfLines-1; i >= 0; i--)
 		{
 			height = UnityEngine.Random.Range(5, 50);
 			instantiatedLine = Instantiate(linePrefab, parentLines.transform);
 			childTransform.x = thickness * 1.05f * ((noOfLines / 2) - i);
 			instantiatedLine.transform.localPosition = childTransform;
-			childScale.x = 1000;
+			childScale.x = thickness*100;
 			childScale.y = height*250;
 			instantiatedLine.transform.localScale = childScale;
 		}
@@ -43,31 +46,42 @@ public class LineMaker : MonoBehaviour
 		{
 			GameObject.Destroy(child.gameObject);
 		}
+		int[] shuffledNumbers = new int[(int)noOfLines];
+		List<int> numbersList = new List<int>((int)noOfLines);
 		childTransform = Vector3.zero;
 		childScale = Vector3.zero;
 		thickness = 10.0f;
 		numbersList.Add(0);
 		numbersList.Clear();
+		if (thickness * 1.05f * noOfLines > 472.5f)
+		{
+			thickness = 472.5f / (noOfLines * 1.05f);
+		}
+		heightMultiplier = 250.0f;
+		if (15000.0f < noOfLines * 250.0f)
+		{
+			heightMultiplier = 15000.0f * 250.0f / (noOfLines * 250.0f);
+		}
 		int[] numbers = new int[(int)noOfLines];
 		for (int i = 1; i <= noOfLines; i++)
 		{
-			numbers[i-1] = i;
+			numbers[i - 1] = i;
 			numbersList.Add(i);
 		}
-		Shuffle<int>(numbers);
+		Shuffle<int>(numbers, shuffledNumbers, numbersList);
 		for (int i = (int)noOfLines - 1; i >= 0; i--)
 		{
 			height = shuffledNumbers[i];
 			instantiatedLine = Instantiate(linePrefab, parentLines.transform);
 			childTransform.x = thickness * 1.05f * ((noOfLines / 2) - i);
 			instantiatedLine.transform.localPosition = childTransform;
-			childScale.x = 1000;
-			childScale.y = height * 250;
+			childScale.x = thickness * 100;
+			childScale.y = height * heightMultiplier;
 			instantiatedLine.transform.localScale = childScale;
 		}
 	}
 
-	private void Shuffle<T>(T[] numbers)
+	private void Shuffle<T>(T[] numbers, int[] shuffledNumbers, List<int> numbersList)
 	{
 		int n = numbers.Length;
 		int r;
