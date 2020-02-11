e7391f8f5e3ecc15f5dc515dcea3764178861b28
shreyanshanchlia <48558967+shreyanshanchlia@users.noreply.github.com>
2020-02-11 21:20:11 +0530

    updated ui

---
+++ Assets/ShowText.cs
@@ -0,0 +1,11 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class ShowText : MonoBehaviour
+{
+    public TextMeshProUGUI text;
+    public void UpdateText(float value)
+    {
+        text.text = ((int)value).ToString();
+    }
+}
