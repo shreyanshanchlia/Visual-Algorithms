891109bf744cd9331c269166ee44864a3a16cc74
shreyanshanchlia <48558967+shreyanshanchlia@users.noreply.github.com>
2020-02-12 02:36:45 +0530

    textbox added
    added textbox for slider

---
+++ Assets/src/ShowText.cs
@@ -0,0 +1,17 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+public class ShowText : MonoBehaviour
+{
+    public TMP_InputField textValue;
+    public Slider NValueSlider;
+    public Image WarningImage;
+    public void updateText(float value)
+    {
+        textValue.text = ((int)value).ToString();
+    }
+    public void UpdateSlider(string value)
+    {
+        NValueSlider.value = int.Parse(value);
+    }
+}
