f1ccf5f323bcbde86fa08a19516327c0e525aae7
shreyanshanchlia <48558967+shreyanshanchlia@users.noreply.github.com>
2020-02-15 13:10:33 +0530

    added magnifier
    Magnifier shows height of lines to be swapped.

--- Assets/src/Height.cs
+++ Assets/src/Height.cs
@@ -7,6 +7,7 @@ public class Height : MonoBehaviour
     public Gradient gradient;
     public TextMeshProUGUI text;
     float scale;
+    private int prevSize;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,6 +17,7 @@ public class Height : MonoBehaviour
             return;
         }
         size = @object.GetComponent<HeightData>().size;
+        prevSize = size;
         scale = @object.transform.localScale.y/size;
         text.text = size.ToString();
     }
@@ -29,6 +31,11 @@ public class Height : MonoBehaviour
             return;
         }
         size = (int)(@object.transform.localScale.y / scale);
+        if (size != prevSize)
+        {
+            @object.GetComponent<HeightData>().size = size;
+            prevSize = size;
+        }
         text.text = size.ToString();
     }
 }
---
+++ Assets/src/HighlightText.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using TMPro;
+using UnityEngine.EventSystems;
+public class HighlightText : MonoBehaviour
+{
+    public GameObject magnifier;
+    public TextMeshProUGUI magnifiedText;
+    private void Update()
+    {
+        Vector3 mousePos = Input.mousePosition;
+        mousePos.z = 10;
+        Vector3 screenPos = Camera.main.ScreenToWorldPoint(mousePos);
+        //raycast and get below text;
+        RaycastHit2D hit = Physics2D.Raycast(screenPos, mousePos);
+
+        if (hit != false && hit.collider != null)
+        {
+            try
+            {
+                magnifier.SetActive(true);
+                magnifiedText.text = hit.collider.gameObject.GetComponent<HeightData>().size.ToString();
+            }
+            catch
+            {
+                magnifier.SetActive(false);
+            }
+        }
+        else
+        {
+            magnifier.SetActive(false);
+        }
+    }
+}
